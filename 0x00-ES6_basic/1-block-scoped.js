0052dd10acab80536d8785aea05bcf238425ac73
siphoPrince <mabirimisesipho@gmail.com>
2024-03-04 04:55:43 -0800

    arrow func

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-	  /*keeping it constant */
+    /*keeping it constant */
     const task = true;
     const task2 = false;
   }
---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = add =>(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
