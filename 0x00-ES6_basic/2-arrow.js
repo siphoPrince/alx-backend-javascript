598c0cca9ec3b808bcadec88a2dcbf1ca72e2f88
siphoPrince <mabirimisesipho@gmail.com>
2024-03-04 04:58:00 -0800

    arrow func

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,7 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = add =>(newNeighborhood) {
+  this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
