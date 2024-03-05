ca4baec859a1954c5c1169c52865176d123d0826
siphoprince <mabirimisesipho@gmail.com>
2024-03-05 13:50:01 +0000

    eslinter

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,23 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true // Add Node.js environment
-    },
-    "extends": "airbnb-base",
-    "parserOptions": {
-        "ecmaVersion": 2021, // Specify ECMAScript version
-        "sourceType": "module"
-    },
-    "rules": {},
-    "overrides": [
-        {
-            "files": ["**/*.js"], // Adjust file pattern if necessary
-            "excludedFiles": ["node_modules/*"], // Exclude node_modules if needed
-            "rules": {
-                // Add specific rules for Node.js files if necessary
-            }
-        }
-    ]
-}
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
 
