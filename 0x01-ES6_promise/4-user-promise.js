725d4122df4b664d5364ac454af9e44903fe990c
siphoprince <mabirimisesipho@gmail.com>
2024-03-06 12:22:03 +0000

    getting api

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,5 @@
-function signUpUser(firstName, lastName) {
+export default function signUpUser(firstName, lastName) {
   return Promise.resolve({
-    firstName: firstName,
-    lastName: lastName
+    firstName, lastName,
   });
 }
