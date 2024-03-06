c05d8d3089d496f51d846866e551562f07e7d41e
siphoprince <mabirimisesipho@gmail.com>
2024-03-06 09:25:34 +0000

    display all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
