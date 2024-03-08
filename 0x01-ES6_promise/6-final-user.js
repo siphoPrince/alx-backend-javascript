3eac000549846972d0f880e3bd5d4f89e57197d4
siphoprince <mabirimisesipho@gmail.com>
2024-03-08 20:04:53 +0000

    trailing arrow

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+
+  return Promise.allSettled(promises).then(results => 
+    results.map(result => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }))
+  );
+}
