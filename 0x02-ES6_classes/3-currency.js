bb951ff76fc642c11868cd8784838c55fe63e68f
siphoprince <mabirimisesipho@gmail.com>
2024-03-08 11:42:50 +0000

    currency lint

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency;
