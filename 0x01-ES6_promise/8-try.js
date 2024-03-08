f807df9527dbb35c30aede98d7ce9c01a6e9997c
siphoprince <mabirimisesipho@gmail.com>
2024-03-08 20:23:10 +0000

    fixing lint code

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -2,6 +2,6 @@ export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw new Error('cannot divide by 0');
   } else {
-      return numerator / denominator;
+    return numerator / denominator;
   }
 }
