438c2c319b9dd5507639b6e3276e70d7f442f1a2
siphoprince <mabirimisesipho@gmail.com>
2024-03-08 11:21:38 +0000

    lint fix

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,51 +1,54 @@
 class HolbertonCourse {
-    constructor(name, length, students) {
-        this._name = this._validateString(name, 'Name');
-        this._length = this._validateNumber(length, 'Length');
-        this._students = this._validateArray(students, 'Students');
-    }
+  constructor(name, length, students) {
+    this._name = this._validateString(name, 'Name');
+    this._length = this._validateNumber(length, 'Length');
+    this._students = this._validateArray(students, 'Students');
+  }
 
-    get name() {
-        return this._name;
-    }
-    set name(newName) {
-        this._name = this._validateString(newName, 'Name');
-    }
+  get name() {
+    return this._name;
+  }
 
-    get length() {
-        return this._length;
-    }
-    set length(newLength) {
-        this._length = this._validateNumber(newLength, 'Length');
-    }
+  set name(newName) {
+    this._name = this._validateString(newName, 'Name');
+  }
 
-    get students() {
-        return this._students;
-    }
-    set students(newStudents) {
-        this._students = this._validateArray(newStudents, 'Students');
-    }
+  get length() {
+    return this._length;
+  }
 
-    _validateString(value, attributeName) {
-        if (typeof value !== 'string') {
-            throw new TypeError(`${attributeName} must be a string`);
-        }
-        return value;
-    }
+  set length(newLength) {
+    this._length = this._validateNumber(newLength, 'Length');
+  }
 
-    _validateNumber(value, attributeName) {
-        if (typeof value !== 'number') {
-            throw new TypeError(`${attributeName} must be a number`);
-        }
-        return value;
-    }
+  get students() {
+    return this._students;
+  }
 
-    _validateArray(value, attributeName) {
-        if (!Array.isArray(value)) {
-            throw new TypeError(`${attributeName} must be an array`);
-        }
-        return value;
+  set students(newStudents) {
+    this._students = this._validateArray(newStudents, 'Students');
+  }
+
+  _validateString(value, attributeName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attributeName} must be a string`);
     }
+    return value;
+  }
+
+  _validateNumber(value, attributeName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${attributeName} must be a number`);
+    }
+    return value;
+  }
+
+  _validateArray(value, attributeName) {
+    if (!Array.isArray(value)) {
+      throw new TypeError(`${attributeName} must be an array`);
+    }
+    return value;
+  }
 }
 
 export default HolbertonCourse;
