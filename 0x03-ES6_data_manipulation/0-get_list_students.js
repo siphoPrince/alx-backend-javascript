648bf3571597d031c22ded1d390c51857162d8aa
siphoPrince <mabirimisesipho@gmail.com>
2024-03-12 04:11:57 -0700

    lint fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
-const getListStudents = () => {
+function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-};
+}
 
 export default getListStudents;
