be80cf6ed14b848452e8fc151eb31c0659dcf053
siphoprince <mabirimisesipho@gmail.com>
2024-03-22 06:16:40 -0700

    bloack scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,6 +3,11 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    return [task, task2];
+    /* eslint-disable */
+    const task = true;
+    const task2 = false;
+    /* eslint-enable */
   }
+
+  return [task, task2];
 }
