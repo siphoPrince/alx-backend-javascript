4c98ce61b0f07c53efeeef2fa670bd06882157c6
siphoprince <mabirimisesipho@gmail.com>
2024-04-30 07:54:12 -0700

    adress in use fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,14 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Holberton School!\n');
+const host_name = '127.0.0.1';
+const port = 1245;
+
+const application = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(1245);
+application.listen(port, host_name);
 
-module.exports = app;
+module.exports = application;
