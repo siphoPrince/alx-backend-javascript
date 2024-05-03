5b805e11b35367598f2f25c5c92fa53c0bc9c40f
siphoprince <mabirimisesipho@gmail.com>
2024-05-03 08:55:21 -0700

    fixing semistandard for task 3

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
 const Utils = require('./utils');
 
-function sendPaymentRequestToApi(totalAmount, totalShipping){
-    const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-    console.log(`The total is: ${res}`);
+function sendPaymentRequestToApi (totalAmount, totalShipping) {
+  const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${res}`);
 }
 
 module.exports = sendPaymentRequestToApi;
